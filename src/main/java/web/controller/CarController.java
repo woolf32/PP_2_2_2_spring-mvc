4ecf9638930039de7f4b153f466396ca60eb73df
йй <woolf_32@mail.ru>
2024-06-20 21:31:57 +0300

    remove @Autowired CarServiceImpl change CarService

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
+import web.service.CarService;
 import web.service.CarServiceImpl;
 
 
@@ -17,8 +18,7 @@ import java.util.List;
 @Controller
 public class CarController {
 
-    @Autowired
-    private CarServiceImpl carServiceImpl;
+    private CarService carServiceImpl;
 
     public CarController(CarServiceImpl carServiceImpl) {
         this.carServiceImpl = carServiceImpl;
